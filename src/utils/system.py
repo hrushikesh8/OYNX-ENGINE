7165933ca44031cd02c1b7a7d0d3225db0d0f918
unknown <hrushikeshbunni88@gmail.com>
2026-02-06 20:48:45 +0530

    system

--- src/utils/system.py
+++ src/utils/system.py
@@ -3,5 +3,5 @@ import shutil
 class SystemUtils:
     @staticmethod
     def check_ffmpeg_availability() -> bool:
-        """Verifies if FFmpeg is installed and accessible."""
+        """Verifies if FFmpeg is installed and accessible.and returns True if it is, otherwise False."""
         return shutil.which("ffmpeg") is not None
